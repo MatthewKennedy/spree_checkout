ec52c03885e322381a1ef3b7ef0ae1cf76f1ebd2
Matthew Kennedy <m.kennedy@me.com>
2022-12-10 09:59:44 +0000

    sort the layout.

--- app/helpers/spree/checkout_helper.rb
+++ app/helpers/spree/checkout_helper.rb
@@ -1,4 +1,9 @@
 module Spree
   module CheckoutHelper
+    def spree_checkout_svg_tag(file_name, options = {})
+      prefixed_file = "spree/checkout/#{file_name}"
+
+      spree_checkout_svg_tag(prefixed_file, options)
+    end
   end
 end
