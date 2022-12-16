ea1f5f652c416f3c4877fbd7174983a711b3669b
Matthew Kennedy <m.kennedy@me.com>
2022-12-16 12:46:48 +0000

    Add card validation.

--- app/javascript/spree/checkout/controllers/index.js
+++ app/javascript/spree/checkout/controllers/index.js
@@ -4,12 +4,14 @@ import { Application } from '@hotwired/stimulus'
 
 // Stimulus - Spree Controllers
 import accordionController from './accordion_controller'
-import FormDisabledController from './form/disabled_controller'
+import InputCardValidationController from './input/card_validation_controller'
+import InputDisabledController from './input/disabled_controller'
 import FormValidationController from './form/validation_controller'
 
 // Stimulus - Setup
 window.Stimulus = Application.start()
 
 Stimulus.register('accordion', accordionController)
-Stimulus.register('input--disable-enable', FormDisabledController)
+Stimulus.register('input--card-validation', InputCardValidationController)
+Stimulus.register('input--disable-enable', InputDisabledController)
 Stimulus.register('form--validation', FormValidationController)
