5f705b8416978e18658a5eed71000dd9be59940a
Matthew Kennedy <m.kennedy@me.com>
2022-12-17 10:30:49 +0000

    Fix address.

--- app/controllers/spree/checkout/orders_controller.rb
+++ app/controllers/spree/checkout/orders_controller.rb
@@ -1,11 +1,8 @@
 module Spree
   module Checkout
-    # This is somewhat contrary to standard REST convention since there is not
-    # actually a Checkout object. There's enough distinct logic specific to
-    # checkout which has nothing to do with updating an order that this approach
-    # is warranted.
     class OrdersController < Spree::Checkout::BaseController
       include Spree::Checkout::AddressBook
+      include Spree::Checkout::StateManagment
 
       before_action :set_cache_header, only: [:edit]
       before_action :set_current_order
@@ -30,6 +27,8 @@ module Spree
 
       layout 'spree/layouts/spree_checkout'
 
+      def edit; end
+
       # Updates the order and advances to the next state (when possible.)
       def update
         if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
@@ -51,10 +50,8 @@ module Spree
         end
       end
 
-      def edit; end
-
       def update_country
-        @order = Spree::Order.new(update_country_params)
+        @order = Spree::Order.new(update_address_country_params)
 
         # Empty out the zipcode and State on Country change.
         @order.send(params[:address_kind]).zipcode = nil
@@ -103,65 +100,8 @@ module Spree
 
       private
 
-      def unknown_state?
-        (params[:state] && !@order.has_checkout_step?(params[:state])) ||
-          (!params[:state] && !@order.has_checkout_step?(@order.state))
-      end
-
-      def insufficient_payment?
-        params[:state] == 'confirm' &&
-          @order.payment_required? &&
-          @order.payments.valid.sum(:amount) != @order.total
-      end
-
-      def correct_state
-        if unknown_state?
-          @order.checkout_steps.first
-        elsif insufficient_payment?
-          'payment'
-        else
-          @order.state
-        end
-      end
-
-      def ensure_valid_state
-        if @order.state != correct_state && !skip_state_validation?
-          flash.keep
-          @order.update_column(:state, correct_state)
-          redirect_to spree.checkout_state_path(@order.state)
-        end
-      end
-
-      # Should be overridden if you have areas of your checkout that don't match
-      # up to a step within checkout_steps, such as a registration step
-      def skip_state_validation?
-        false
-      end
-
-      def load_order_with_lock
-        @order = current_order(lock: true)
-        redirect_to(spree.cart_path) && return unless @order
-      end
-
-      def ensure_valid_state_lock_version
-        if params[:order] && params[:order][:state_lock_version]
-          changes = @order.changes if @order.changed?
-          @order.reload.with_lock do
-            unless @order.state_lock_version == params[:order].delete(:state_lock_version).to_i
-              flash[:error] = Spree.t(:order_already_updated)
-              redirect_to(checkout_state_path(@order.state)) && return
-            end
-            @order.increment!(:state_lock_version)
-          end
-          @order.assign_attributes(changes) if changes
-        end
-      end
-
-      def set_state_if_present
-        if params[:state]
-          redirect_to spree.checkout_state_path(@order.state) if @order.can_go_to_state?(params[:state]) && !skip_state_validation?
-          @order.state = params[:state]
-        end
+      def update_address_country_params
+        params.fetch(:order, {}).permit(permitted_order_attributes)
       end
 
       def ensure_checkout_allowed
@@ -177,50 +117,6 @@ module Spree
           flash[:error] = Spree.t(:inventory_error_flash_for_insufficient_quantity)
           redirect_to spree.cart_path
         end
-      end
-
-      # Provides a route to redirect after order completion
-      def completion_route(custom_params = {})
-        spree.order_path(@order, custom_params.merge(locale: locale_param))
-      end
-
-      def setup_for_current_state
-        method_name = :"before_#{@order.state}"
-        send(method_name) if respond_to?(method_name, true)
-      end
-
-      def before_address
-        # if the user has a default address, a callback takes care of setting
-        # that; but if he doesn't, we need to build an empty one here
-        @order.bill_address ||= Address.new(country: current_store.default_country, user: try_spree_current_user)
-        if @order.checkout_steps.include?('delivery')
-          @order.ship_address ||= Address.new(country: current_store.default_country,
-                                              user: try_spree_current_user)
-        end
-
-        @bill_address ||= @order.bill_address
-        @ship_address ||= @order.ship_address
-      end
-
-      def before_delivery
-        return if params[:order].present?
-
-        packages = @order.shipments.map(&:to_package)
-        @differentiator = Spree::Stock::Differentiator.new(@order, packages)
-      end
-
-      def before_payment
-        if @order.checkout_steps.include? 'delivery'
-          packages = @order.shipments.map(&:to_package)
-          @differentiator = Spree::Stock::Differentiator.new(@order, packages)
-          @differentiator.missing.each do |variant, quantity|
-            Spree::Dependencies.cart_remove_item_service.constantize.call(order: @order, variant: variant, quantity: quantity)
-          end
-        end
-
-        return unless try_spree_current_user.respond_to?(:payment_sources)
-
-        @payment_sources = try_spree_current_user.payment_sources.where(payment_method: @order.available_payment_methods)
       end
 
       def add_store_credit_payments
@@ -264,10 +160,6 @@ module Spree
 
       def remove_store_credit_service
         Spree::Dependencies.checkout_remove_store_credit_service.constantize
-      end
-
-      def update_country_params
-        params.fetch(:order, {}).permit(permitted_order_attributes)
       end
 
       def coupon_handler
