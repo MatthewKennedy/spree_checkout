101b5db85af4ce2749c96f82aa7b34ce4b9b5417
Matthew Kennedy <m.kennedy@me.com>
2022-12-21 17:00:57 +0000

    Remove unneeded base JavaScript.

--- app/assets/javascripts/spree_checkout.js
+++ app/assets/javascripts/spree_checkout.js
@@ -12859,34 +12859,6 @@
   Stimulus.register("form--validation", FormValidationController);
   Stimulus.register("modal", ModalController);
   Stimulus.register("toast", ToastController);
-  const SpreeCheckout = {};
-  if (!window.SpreeCheckout) {
-    window.SpreeCheckout = SpreeCheckout;
-  }
-  const platformApiMountedAt = function() {
-    return window.SpreeCheckout.paths.platform_api_mounted_at;
-  };
-  const pathFor = function(path) {
-    const locationOrigin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ":" + window.location.port : "");
-    const queryParts = window.location.search;
-    const uri = `${locationOrigin + platformApiMountedAt() + path + queryParts}`;
-    return uri;
-  };
-  SpreeCheckout.localizedPathFor = function(path) {
-    const defaultLocale = this.localization.default_locale;
-    const currentLocale = this.localization.current_locale;
-    const defaultCurrency = this.localization.default_currency;
-    const currentCurrency = this.localization.current_currency;
-    if (defaultLocale !== currentLocale || defaultCurrency !== currentCurrency) {
-      const fullUrl = new URL(pathFor(path));
-      const params = fullUrl.searchParams;
-      const pathName = fullUrl.pathname;
-      params.set("locale", currentLocale);
-      params.set("currency", currentCurrency);
-      return fullUrl.origin + pathName + "?" + params.toString();
-    }
-    return pathFor(path);
-  };
   console.log("Spree Checkout Initialized");
   var adapters = {
     logger: self.console,
--- app/javascript/spree/checkout/index.js
+++ app/javascript/spree/checkout/index.js
@@ -1,6 +1,5 @@
 import '@hotwired/turbo-rails'
 import './vendors'
 import './controllers'
-import './utilities'
 
 console.log('Spree Checkout Initialized')
