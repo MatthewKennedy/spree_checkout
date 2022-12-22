bc136fe1e3cd4b75fc5781ef8caa6a2dd8ba0b88
Matthew Kennedy <m.kennedy@me.com>
2022-12-22 21:07:11 +0000

    Fix setting on store.

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   module StoreDecorator
     def self.prepended(base)
-      base.typed_store :settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
+      base.typed_store :spree_checkout_settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
         s.boolean :checkout_shipping_instructions, default: true, null: false
         s.boolean :checkout_coupon_codes_enabled, default: true, null: true
       end
