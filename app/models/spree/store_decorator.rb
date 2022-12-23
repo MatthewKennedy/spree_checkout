6aa92f9e1c7e3794609c457bf30ca9d11a09cd3b
Matthew Kennedy <m.kennedy@me.com>
2022-12-23 14:15:19 +0000

    Move more configs into store settings.

--- app/models/spree/store_decorator.rb
+++ app/models/spree/store_decorator.rb
@@ -4,6 +4,9 @@ module Spree
       base.typed_store :spree_checkout_settings, coder: ActiveRecord::TypedStore::IdentityCoder do |s|
         s.boolean :checkout_shipping_instructions, default: true, null: false
         s.boolean :checkout_coupon_codes_enabled, default: true, null: true
+        s.boolean :checkout_alternative_shipping_phone, default: false, null: true
+        s.boolean :checkout_allow_guest_checkout, default: true, null: true
+        s.boolean :checkout_address_requires_phone, default: true, null: true
       end
     end
 
