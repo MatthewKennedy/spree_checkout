74d5c55ec499dae3e43f42a55e4d6e804fe9400a
Matthew Kennedy <m.kennedy@me.com>
2022-12-25 23:38:25 +0000

    fix configs.

--- config/initializers/spree_checkout_paths.rb
+++
@@ -1,2 +0,0 @@
-Rails.configuration.x.spree_checkout.orders_path_name = :orders
-Rails.configuration.x.spree_checkout.cart_path_name = :cart
--- lib/spree_checkout.rb
+++ lib/spree_checkout.rb
@@ -1,1 +1,15 @@
 require "spree/checkout"
+
+module SpreeCheckout
+  class << self
+    def configuration
+      @configuration ||= Spree::Checkout::Configuration.new
+    end
+
+    alias_method :config, :configuration
+
+    def configure
+      yield configuration
+    end
+  end
+end
